5f6c860b4d089623daf7bc5030cbca5f636511af
Thomas Kelly <tmkelly28@gmail.com>
2018-03-22 16:49:30 -0400

    remove nav

--- app/index.js
+++ app/index.js
@@ -20,9 +20,6 @@ class Counter extends React.Component {
   render () {
     return (
       <div id='container'>
-        <div id='navbar'>
-          Counter.js
-        </div>
         <div id='counter'>
           <h1>{this.state.count}</h1>
           <button onClick={this.increment}>Increment</button>
