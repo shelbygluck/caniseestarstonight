ce8ba07969877e9264654334b7c26d4ead6f9c00
Shelby Gluck <shelbygluck@gmail.com>
2020-01-28 10:21:20 -0500

    deploy script

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express')
+const app = express()
+const path = require('path')
 
-app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname));
-app.use("/images", express.static(__dirname + '/app'));
+app.use(express.static(path.join(__dirname)))
+app.use("/styles", express.static(__dirname))
+app.use("/images", express.static(__dirname + '/app'))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
-});
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
 
+app.get('/favicon.ico', function (req, res) {
+    res.sendFile(path.join(__dirname + '/favicon.ico'));
+  });
 
 app.listen(process.env.PORT || 8080);
